700a80aa22ce00042fd00fd0052b678919672e7a
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-04 16:46:38 +0500

    ADD TALIWIND STYLE

--- src/components/users/User.tsx
+++ src/components/users/User.tsx
@@ -13,11 +13,11 @@ type Props = {
 
 export default function User({ item }: Props) {
     return (
-        <div className="w-1/6 p-5 shadow-lg cursor-pointer flex flex-col justify-center items-center gap-3 rounded-md">
-            <div className="">{item.id}</div>
-            <UserInfo key='User Name' value={item.username}/>
-            <UserInfo key='User Phone'  value={item.phone}/>
-            <UserInfo key='User Email' value={item.email}/>
+        <div className="group  w-1/6 p-5 shadow-lg cursor-pointer flex flex-col justify-center items-center gap-3 rounded-md hover:bg-slate-400 transition-bg ease duration-300 hover:-translate-y-5  ">
+            <div className="group-hover:text-white">{item.id}</div>
+            <UserInfo title='User Name' value={item.username}/>
+            <UserInfo title='User Phone'  value={item.phone}/>
+            <UserInfo title='User Email' value={item.email}/>
         </div>
     );
 }
--- src/components/users/UserInfo.tsx
+++ src/components/users/UserInfo.tsx
@@ -1,13 +1,13 @@
 type Props = {
-    key: string;
+    title: string;
     value: string | number;
 };
 
-export default function UserInfo({ key, value }: Props) {
+export default function UserInfo({ title, value }: Props) {
     return (
         <div className="flex justify-center items-center gap-2">
-            <span className="font-bold">{key}: </span>
-            <p className="text-gray-500">{value}</p>
+            <span className="font-bold  group-hover:text-white">{title}: </span>
+            <p className="text-gray-500  group-hover:text-white">{value}</p>
         </div>
     );
 }
